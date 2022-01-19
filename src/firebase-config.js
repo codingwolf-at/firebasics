d49ea982e748259b835a6e4e896dc91b4b7b57be
codingwolf-at <atameshwari9@gmail.com>
2022-01-19 18:57:22 +0530

    add: firebase config file

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsC6tV30HzPSXK24qoPSQoo6SEzKVDtIQ",
+  authDomain: "firebasics-d510c.firebaseapp.com",
+  projectId: "firebasics-d510c",
+  storageBucket: "firebasics-d510c.appspot.com",
+  messagingSenderId: "47043386430",
+  appId: "1:47043386430:web:04d5f38cc9c6ab3f75c56f",
+  measurementId: "G-ZF7RM9DQ82"
+};
+
+const app = initializeApp(firebaseConfig);
