a8d3fa54207ba305ab0c10db59ca13f002332a41
codingwolf-at <atameshwari9@gmail.com>
2022-01-20 18:57:14 +0530

    add: header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello react</h1>
+      <nav class="nav nav-primary p-1">
+        <span class="nav-brand">Firebasics</span>
+        <div class="nav-link-container">
+          <span class="nav-link">Home</span>
+          <span class="nav-link">Contact</span>
+          <span class="nav-link">Services</span>
+        </div>
+      </nav>
     </div>
   );
 }
